48c164a67d62fb87ee37b0673f228c326e77656f
Marc Fornos <frozen.pomelo@gmail.com>
2022-06-10 18:41:39 +0200

    add data refreshing

--- explorer-ui/src/components/LatestContractActivity.tsx
+++ explorer-ui/src/components/LatestContractActivity.tsx
@@ -1,10 +1,10 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useQuery } from "urql"
 import { Activity } from "../types/contracts"
 import List from "./List"
 import ActivityRow from "./substrate/ActivityRow"
 
-export const LatestContractActivityQuery = `
+export const LATEST_ACTIVITIES = `
 query LatestContractActivity {
   activities(orderBy: createdAt_DESC, limit: 5) {
     action
@@ -23,18 +23,29 @@ query LatestContractActivity {
 `
 
 export default function LatestContractActivity () {
-  const [result] = useQuery({
-    query: LatestContractActivityQuery
+  const [result, reexecuteQuery] = useQuery({
+    query: LATEST_ACTIVITIES
   })
+
+  useEffect(() => {
+    if (result.fetching) return
+
+    // Refresh every second...
+    const timerId = setInterval(() => {
+      reexecuteQuery({ requestPolicy: "cache-and-network" })
+    }, 1000)
+
+    return () => clearTimeout(timerId)
+  }, [result.fetching, reexecuteQuery])
 
   const { data, fetching, error } = result
 
+  const items = data?.activities.map((activity : Activity) => (
+    <ActivityRow key={activity.id} activity={activity} />
+  ))
+
   if (fetching) return <p>Loading...</p>
   if (error) return <p>Oh no... {error.message}</p>
-
-  const items = data.activities.map((activity : Activity) => {
-    return <ActivityRow key={activity.id} activity={activity} />
-  })
 
   return (
     <List title="Latest Transactions" description="Contract related extrinsics" items={items} />
