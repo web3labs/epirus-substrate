0b6119c3144a6bdae67d6d3f76306247895c9171
Marc Fornos <frozen.pomelo@gmail.com>
2022-07-07 16:25:40 +0200

    default pull interval to 10s

--- explorer-ui/src/hooks/useSquid.ts
+++ explorer-ui/src/hooks/useSquid.ts
@@ -16,7 +16,7 @@ interface Props {
 
 export default function useSquid ({
   query, variables, refresh = {
-    millis: 1000,
+    millis: 10000,
     disabled: false
   }
 }: Props) : [UseQueryState, () => void] {
