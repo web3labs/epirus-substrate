a12327782fa5aee1b3217fc6208a35095f6362da
Marc Fornos <frozen.pomelo@gmail.com>
2022-07-12 11:09:41 +0200

    custom breadcrumb

--- explorer-ui/src/formats/text.ts
+++ explorer-ui/src/formats/text.ts
@@ -1,14 +1,22 @@
-export function shortenHexString (text?: string, ellipsis: string = "..") {
+export function shortenHexString (text?: string, ellipsis: string = "…") {
   return shorten(text, 8, 6, ellipsis)
 }
 
-export function shorten (text?: string, start: number = 4, end: number = 4, ellipsis: string = "..") {
+export function shorten (text?: string, start: number = 4, end: number = 4, ellipsis: string = "…") {
   if (text) {
     return text.substring(0, start) +
             ellipsis +
             text.substring(text.length - end)
   } else {
     return null
+  }
+}
+
+export function shortenStart (text?: string, max: number = 16, ellipsis: string = "…") {
+  if (text && text.length > max) {
+    return text.substring(0, max) + ellipsis
+  } else {
+    return text
   }
 }
 
