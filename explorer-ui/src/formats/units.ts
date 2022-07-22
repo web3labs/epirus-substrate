c2afe50c0e60cb2708b488e1aab2c429b39486b2
xueying <wang.xy.326@gmail.com>
2022-07-22 15:44:14 +0200

    Display currency without trailing zeros

--- explorer-ui/src/formats/units.ts
+++ explorer-ui/src/formats/units.ts
@@ -4,9 +4,60 @@ export function formatUnits (
   amount: number | string | undefined,
   token: {tokenDecimals: number, tokenSymbol:string}
 ) {
-  return formatBalance(amount || 0, {
-    decimals: token.tokenDecimals,
-    withUnit: token.tokenSymbol,
-    forceUnit: token.tokenSymbol
-  })
+  const amt = amountToString(amount)
+  const formatted = amt.length > 8
+    ? formatBalance(amt, {
+      decimals: token.tokenDecimals,
+      withUnit: token.tokenSymbol,
+      forceUnit: token.tokenSymbol
+    })
+    : formatSmallBalance(amt, { decimals: token.tokenDecimals, symbol: token.tokenSymbol })
+  return removeTrailingZeros(formatted)
+}
+
+/**
+ * For formatting very small units of currency that show as 0 using formatBalance of @polkadotjs/utils
+ * We take just the first 2 digits of the amount since there would already be many zeros leading up to the decimal point
+ * @param amount amount in Planck
+ * @param tokenConfig currency config
+ * @returns formatted amount with currency symbol
+ */
+function formatSmallBalance (amount: string, tokenConfig: { decimals: number, symbol: string }): string {
+  if (amount === "0") {
+    return [amount, tokenConfig.symbol].join(" ")
+  }
+  const numberOfLeadingZeros = tokenConfig.decimals - amount.length
+  const decimalArray = Array(numberOfLeadingZeros).fill("0")
+  decimalArray.push(amount)
+  return `0.${decimalArray.join("").substring(0, numberOfLeadingZeros + 2)} ${tokenConfig.symbol}`
+}
+
+function amountToString (amount: string | number | undefined): string {
+  let amt = amount || 0
+  if (typeof amt === "number") {
+    amt = amt.toString()
+  }
+  return amt
+}
+
+function removeTrailingZeros (amount: string): string {
+  const [amt, symbol] = amount.split(" ")
+  const [whole, decimals] = amt.split(".")
+
+  if (decimals === undefined) {
+    return amount
+  }
+
+  const decArray = decimals.split("")
+  for (let i = decArray.length - 1; i >= 0; i--) {
+    if (decimals[i] !== "0") {
+      break
+    }
+    decArray.pop()
+  }
+  if (decArray.length === 0) {
+    return [whole, symbol].join(" ")
+  }
+
+  return `${whole}.${decArray.join("")} ${symbol}`
 }
