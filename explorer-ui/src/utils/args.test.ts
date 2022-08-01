920522a681f17dc5fdf11fba72c883756bb73a7e
Marc Fornós <frozen.pomelo@gmail.com>
2022-08-01 17:48:08 +0200

    add utils args tests

--- explorer-ui/src/utils/args.test.ts
+++ explorer-ui/src/utils/args.test.ts
@@ -1,5 +1,25 @@
+import { Args } from "../types/extrinsic"
 import { getArgValue } from "./args"
 
 test("Undefined args", () => {
-  expect(getArgValue(null, "lol")).toBeUndefined()
+  expect(getArgValue(null, "lol")).toBeNull()
+})
+
+test("Default value arg", () => {
+  expect(getArgValue({ value: "mike" })).toBe("mike")
+})
+
+test("Zero value args", () => {
+  const testArgs = {
+    a: "0",
+    b: "0"
+  } as Args
+
+  expect(getArgValue(testArgs, "b")).toBeNull()
+})
+
+test("Simple string arg", () => {
+  expect(getArgValue({
+    name: "xoco"
+  }, "name")).toBe("xoco")
 })
