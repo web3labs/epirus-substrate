2eede5e7cc7c5da8cc73ba4b71f32110f69d769a
Marc Fornós <frozen.pomelo@gmail.com>
2022-08-02 18:12:37 +0200

    add current id test cases

--- explorer-ui/src/components/accounts/AccountLink.test.tsx
+++ explorer-ui/src/components/accounts/AccountLink.test.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import AccountLink from "./AccountLink"
+import { randomAccountId } from "../../_mocks/utils"
 
 describe("AccountLink component", () => {
   it("should render an account link", () => {
@@ -17,5 +18,58 @@ describe("AccountLink component", () => {
 
     expect(link.length).toBe(1)
     expect(link[0].href).toBe("http://localhost/accounts/5HdKDnfR2X8y4fkgQUxXJuBxu638PuKQGUy5G16cyTjT5RzL")
+  })
+
+  it("should add a badge if it is a contract account", () => {
+    const {
+      container
+    } = render(
+      <MemoryRouter initialEntries={["/accounts"]}>
+        <AccountLink
+          account={{
+            id: "5HdKDnfR2X8y4fkgQUxXJuBxu638PuKQGUy5G16cyTjT5RzL",
+            contract: {
+              id: randomAccountId()
+            }
+          }}
+        />
+      </MemoryRouter>)
+
+    expect(container.getElementsByTagName("a").length).toBe(1)
+    expect(container.getElementsByTagName("svg").length).toBeGreaterThanOrEqual(1)
+  })
+
+  it("should not render a link if it is the current id", () => {
+    const {
+      container
+    } = render(
+      <MemoryRouter initialEntries={["/accounts"]}>
+        <AccountLink
+          account={{ id: "5HdKDnfR2X8y4fkgQUxXJuBxu638PuKQGUy5G16cyTjT5RzL" }}
+          currentId="5HdKDnfR2X8y4fkgQUxXJuBxu638PuKQGUy5G16cyTjT5RzL"
+        />
+      </MemoryRouter>)
+
+    expect(container.getElementsByTagName("a").length).toBe(0)
+  })
+
+  it("should add a badge if it is a contract account without link", () => {
+    const {
+      container
+    } = render(
+      <MemoryRouter initialEntries={["/accounts"]}>
+        <AccountLink
+          account={{
+            id: "5HdKDnfR2X8y4fkgQUxXJuBxu638PuKQGUy5G16cyTjT5RzL",
+            contract: {
+              id: randomAccountId()
+            }
+          }}
+          currentId="5HdKDnfR2X8y4fkgQUxXJuBxu638PuKQGUy5G16cyTjT5RzL"
+        />
+      </MemoryRouter>)
+
+    expect(container.getElementsByTagName("a").length).toBe(0)
+    expect(container.getElementsByTagName("svg").length).toBeGreaterThanOrEqual(1)
   })
 })
