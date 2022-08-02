c151513277a085d2127a53908c42b4539fbd3c20
Marc Fornós <frozen.pomelo@gmail.com>
2022-08-02 17:01:44 +0200

    use describe for grouping

--- explorer-ui/src/components/accounts/AccountLink.test.tsx
+++ explorer-ui/src/components/accounts/AccountLink.test.tsx
@@ -3,17 +3,19 @@ import { render } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import AccountLink from "./AccountLink"
 
-test("Account link contains a link", () => {
-  const {
-    container
-  } = render(
-    <MemoryRouter initialEntries={["/accounts"]}>
-      <AccountLink
-        account={{ id: "5HdKDnfR2X8y4fkgQUxXJuBxu638PuKQGUy5G16cyTjT5RzL" }}
-      />
-    </MemoryRouter>)
-  const link = container.getElementsByTagName("a")
+describe("AccountLink component", () => {
+  it("should render an account link", () => {
+    const {
+      container
+    } = render(
+      <MemoryRouter initialEntries={["/accounts"]}>
+        <AccountLink
+          account={{ id: "5HdKDnfR2X8y4fkgQUxXJuBxu638PuKQGUy5G16cyTjT5RzL" }}
+        />
+      </MemoryRouter>)
+    const link = container.getElementsByTagName("a")
 
-  expect(link.length).toBe(1)
-  expect(link[0].href).toBe("http://localhost/accounts/5HdKDnfR2X8y4fkgQUxXJuBxu638PuKQGUy5G16cyTjT5RzL")
+    expect(link.length).toBe(1)
+    expect(link[0].href).toBe("http://localhost/accounts/5HdKDnfR2X8y4fkgQUxXJuBxu638PuKQGUy5G16cyTjT5RzL")
+  })
 })
