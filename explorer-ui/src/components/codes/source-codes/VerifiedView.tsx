f49eeeaa49364db85611f3d4af59f02fe9c71cda
Xueying Wang <wang.xy.326@gmail.com>
2022-10-28 09:47:44 +0200

    simple verified view

--- explorer-ui/src/components/codes/source-codes/VerifiedView.tsx
+++ explorer-ui/src/components/codes/source-codes/VerifiedView.tsx
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useChainProperties } from "../../../contexts/ChainContext"
 import api from "./verifierApi"
-// import { ArrowsPointingInIcon, ArrowsPointingOutIcon } from "@heroicons/react/24/outline"
-// import hljs from "highlight.js/lib/common"
-// import "highlight.js/styles/github.css"
-// import Copy from "../../commons/Copy"
-// import { classNames } from "../../../utils/strings"
 
 export default function VerifiedView ({
   id
@@ -19,7 +14,7 @@ export default function VerifiedView ({
     async function loadSource () {
       try {
         const data = await api.resources({ chain: info, codeHash: id })
-        console.log(data)
+
         setSource(data)
       } catch (error) {
         // TODO: handle error
@@ -34,40 +29,4 @@ export default function VerifiedView ({
   return (
     <div>Verified</div>
   )
-
-  // function toggleExpanded () {
-  //   setExpanded(prev => !prev)
-  // }
-
-  // const highlightedJson = hljs.highlight(JSON.stringify(abi, null, 2), { language: "json" }).value
-  // const highlightedInk = hljs.highlight(source, { language: "rust" }).value
-  // return (
-  //   <>
-  //     <div className="px-4 pt-4 pb-2 flex text-neutral-500">
-  //       <div className="pr-2 text-sm font-semibold">Contract Source Code</div>
-  //     </div>
-
-  //     <div className="my-2 mx-4 border border-neutral-200 rounded text-xs">
-  //       <div className="p-2 flex justify-between bg-neutral-100 items-center">
-  //         <div className="font-mono">lib.rs</div>
-  //         <div className="grid grid-cols-2 gap-2 items-center">
-  //           <Copy text={source}/>
-  //           <div className="bg-neutral-100 rounded-full p-1 cursor-pointer hover:bg-neutral-200">
-  //             {
-  //               expanded
-  //                 ? <ArrowsPointingInIcon width={18} height={18} onClick={toggleExpanded}/>
-  //                 : <ArrowsPointingOutIcon width={18} height={18} onClick={toggleExpanded}/>
-  //             }
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <pre className={classNames(
-  //         expanded ? "" : "break-normal max-h-80 overflow-y-auto scroll-smooth",
-  //         "github p-2"
-  //       )}>
-  //         <code dangerouslySetInnerHTML={{ __html: highlightedInk }}/>
-  //       </pre>
-  //     </div>
-  //   </>
-  // )
 }
