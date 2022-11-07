fa869f44f1daa29c04b68a16087aa38be5a29c6b
Xueying Wang <wang.xy.326@gmail.com>
2022-11-07 12:52:06 +0100

    metadata file view & tooltip on verification type

--- explorer-ui/src/components/codes/sources/MetadataView.tsx
+++ explorer-ui/src/components/codes/sources/MetadataView.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable camelcase */
 import { InformationCircleIcon, ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/24/outline"
+import hljs from "highlight.js"
+import "highlight.js/styles/github.css"
 import React, { useState } from "react"
+import Copy from "../../commons/Copy"
 import { Definition } from "../../commons/Definitions"
 
 export interface ContractMetadata {
@@ -34,13 +37,21 @@ export default function MetadataView (
   { metadata } :
   { metadata: ContractMetadata }
 ) {
-  const [open, setOpen] = useState(false)
+  const [buildInfoOpen, setBuildInfoOpen] = useState(false)
+  const [metadataOpen, setMetadataOpen] = useState(false)
   const { contract, source } = metadata
   const { build_info } = source
+  const metadataAsString = JSON.stringify(metadata, null, 2)
 
-  function toggleOpen () {
-    setOpen(prev => !prev)
+  function toggleBuildInfoOpen () {
+    setBuildInfoOpen(prev => !prev)
   }
+  function toggleMetadataOpen () {
+    setMetadataOpen(prev => !prev)
+  }
+
+  const { value } = hljs.highlightAuto(metadataAsString, ["rust", "rs", "toml", "json", "text"])
+
   return (
     <div className="flex flex-col gap-4 text-gray-700">
       <div className="flex flex-col gap-2">
@@ -58,7 +69,15 @@ export default function MetadataView (
             <Definition label="Verification" term={
               <div className="flex gap-1 items-center">
                 <span>Bytecode Match</span>
-                <InformationCircleIcon className="h-4 w-4"/>
+                <div className="flex items-center group">
+                  <InformationCircleIcon height={18} width={18}/>
+                  <span
+                    className="absolute mx-5 p-2 text-xs text-gray-50 border rounded border-gray-700
+                     bg-gray-700 text-wrap max-w-xs invisible group-hover:visible"
+                  >
+                    The source code files were compiled and the output matched against the Wasm blob deployed on-chain.
+                  </span>
+                </div>
               </div>
             }/>
           </div>
@@ -74,13 +93,13 @@ export default function MetadataView (
       </div>
 
       <div className="flex flex-col gap-2">
-        <div className="flex cursor-pointer items-center" onClick={toggleOpen}>
-          <div className="text-sm font-semibold text-gray-500">Build Information</div>
-          {open
+        <div className="flex gap-1 cursor-pointer items-center" onClick={toggleBuildInfoOpen}>
+          {buildInfoOpen
             ? <ChevronDownIcon className="h-4 w-4"/>
             : <ChevronRightIcon className="h-4 w-4"/> }
+          <div className="text-sm font-semibold text-gray-500">Build Information</div>
         </div>
-        { open &&
+        { buildInfoOpen &&
         <div className="flex flex-col gap-2">
           <table className="w-1/3 table-auto border-collapse border border-slate-300 text-xs">
             <tbody>
@@ -102,6 +121,28 @@ export default function MetadataView (
               </tr>
             </tbody>
           </table>
+        </div>
+        }
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-1 cursor-pointer items-center" onClick={toggleMetadataOpen}>
+          {metadataOpen
+            ? <ChevronDownIcon className="h-4 w-4"/>
+            : <ChevronRightIcon className="h-4 w-4"/> }
+          <div className="text-sm font-semibold text-gray-500">Contract Metadata</div>
+        </div>
+        { metadataOpen &&
+        <div className="my-2 border border-neutral-200 rounded text-xs">
+          <div className="p-2 flex justify-between bg-neutral-100 items-center">
+            <div className="font-mono">metadata.json</div>
+            <div className="flex gap-1 items-center">
+              <Copy text={metadataAsString}/>
+            </div>
+          </div>
+          <pre className="github p-2 overflow-y-auto scroll-smooth">
+            <code dangerouslySetInnerHTML={{ __html: value }}/>
+          </pre>
         </div>
         }
       </div>
