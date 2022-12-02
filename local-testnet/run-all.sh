d4c81de28ce0ac3a7c2e4171b69571952c499626
Xueying Wang <wang.xy.326@gmail.com>
2022-12-02 18:09:40 +0100

    pull images before running in local testnet

--- local-testnet/run-all.sh
+++ local-testnet/run-all.sh
@@ -8,4 +8,10 @@ docker-compose \
 -f docker-compose.squid-archive.yml \
 -f docker-compose.squid-ink.yml \
 -f docker-compose.explorer-ui.yml \
+pull && \
+docker-compose \
+-f docker-compose.testnet.yml \
+-f docker-compose.squid-archive.yml \
+-f docker-compose.squid-ink.yml \
+-f docker-compose.explorer-ui.yml \
 up
