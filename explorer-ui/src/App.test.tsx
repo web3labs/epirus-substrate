4175833f814ab090063f28ceadcedc6dbf0d2fab
Marc Fornos <frozen.pomelo@gmail.com>
2023-02-08 11:57:03 +0100

    add screen width to test

--- explorer-ui/src/App.test.tsx
+++ explorer-ui/src/App.test.tsx
@@ -1,8 +1,11 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import App from "./App"
+import { setScreenWidth } from "./_mocks/media"
 
 test("blockchain link should be in the home page", () => {
+  setScreenWidth()
+
   render(<App />)
   const linkElement = screen.getByText(/Blockchain/i)
   expect(linkElement).toBeInTheDocument()
