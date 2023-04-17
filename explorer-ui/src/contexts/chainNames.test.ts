b722cc75b0b62fa5081491c0447328d93b54207a
abhi <abhijeet.bhagat@gmx.com>
2023-04-17 21:02:25 +0530

    Fix az verification endpoint (#6)
    * add aleph to list of chain names
    
    * fix azero testnet endpoint name according to the latest

--- explorer-ui/src/contexts/chainNames.test.ts
+++ explorer-ui/src/contexts/chainNames.test.ts
@@ -4,11 +4,15 @@ test("Unknown chain", () => {
   expect(() => resolveInfoFromName("no name")).toThrowError()
 })
 
+test("alephzero chain info", () => {
+  expect(resolveInfoFromName("aleph zero")).toEqual("aleph")
+})
+
 test("Local chain info", () => {
   expect(resolveInfoFromName("local contracts chain")).toEqual("local")
 })
 
-test("Rococo chain ingo", () => {
+test("Rococo chain info", () => {
   expect(resolveInfoFromName("contracts on rococo")).toEqual("rococoContracts")
 })
 
